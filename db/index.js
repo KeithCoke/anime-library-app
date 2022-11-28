e3c328563bb4805e5eb6af2841b0f35f433d8578
KeithCoke <littlecokers@gmail.com>
2022-11-27 22:30:53 -0800

    db folder created

---
+++ db/index.js
@@ -0,0 +1,22 @@
+// dependencies
+const mongoose = require("mongoose");
+console.log()
+// connect to MongoDB via mongoose
+const connectionString = process.env.MONGODB_URI
+mongoose.connect(
+    connectionString,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+);
+
+// console.log() connection status
+mongoose.connection.on('connected', () => {
+    console.log('mongoose connected to ', connectionString);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('mongoose disconnected to ', connectionString);
+});
+
+mongoose.connection.on('error', (error) => {
+    console.log('mongoose error ', error);
+});
--- server.js
+++ server.js
@@ -4,10 +4,12 @@
 const express = require('express');
 const app = express();
 const methodOverride = require('method-override');
-const port = 3000
+require("dotenv").config();
 
-const db = require('./models')
-const anime = require('./models')
+const port = process.env.PORT
+
+const db = require('./db')
+// const anime = require('./models/anime')
 
 // +-+-+-+-+-+-+-+-+-+-+
 // |M|I|D|D|L|E|W|A|R|E|
@@ -20,17 +22,19 @@ app.use(express.static('public'))
 // |R|O|U|T|E|S|
 // +-+-+-+-+-+-+
 
-
-app.get('/anime', (req, res) => {
-	db.anime.find({}, (err, animes) => {
+app.get('/', (req, res)=> {
+    res.render('index.ejs')
+})
+//  app.get('/anime', (req, res) => {
+// 	db.anime.find({}, (err, animes) => {
 
        
-            res.render('home.ejs', {
-				animes: animes,
-				tabTitle: 'Master Branch'
-			})
-        })
-	})
+//             res.render('home.ejs', {
+// 				animes: animes,
+// 				tabTitle: 'Master Branch'
+// 			})
+//         })
+// 	})
 
 // +-+-+-+-+-+-+-+-+
 // |L|I|S|T|E|N|E|R|
