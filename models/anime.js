f8a98166260260a19ef64c172be5ec50995b4ff3
KeithCoke <littlecokers@gmail.com>
2022-11-30 09:25:24 -0800

    new anime error debugged

--- models/anime.js
+++ models/anime.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 const animeSchema = new Schema(
     {
         name: { type: String, required: true },
-        year: { type: Number, required: true },
+        year: { type: Number, default: 0000 },
         image: { type: String, default: 'https://www.equalserving.com/images/es1scplugin/image-not-available' }
     }
 )
